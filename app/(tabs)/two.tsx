264e7bb114534da2073ae433cb483a693d87bbdb
Christopher Obocha <christopherobocha@gmail.com>
2025-02-11 11:23:20 +0000

    WIP

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Image } from 'expo-image';
 import * as Location from 'expo-location';
 import { Stack } from 'expo-router';
@@ -10,38 +11,82 @@ import { Spacer } from '~/utils/Spacer';
 import { constants, Hotel } from '~/utils/constants';
 
 const { DEFAULT_HOTELS } = constants;
+type Category = 'hotels' | 'restaurants' | 'geos' | 'attractions';
 
 export default function Home() {
   const hotelData = DEFAULT_HOTELS;
+  const defaultLocation: Location.LocationObject = {
+    coords: {
+      latitude: 42.3455,
+      longitude: -71.10767,
+      altitude: null,
+      accuracy: null,
+      altitudeAccuracy: null,
+      heading: null,
+      speed: null,
+    },
+    timestamp: new Date().getTime(),
+  };
   const insets = useSafeAreaInsets();
-  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [location, setLocation] = useState<Location.LocationObject | null>(defaultLocation);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [hotels, setHotels] = useState<any>();
 
   const backgroundContainer = {
     paddingTop: insets.top + 10,
   };
 
+  // useEffect(() => {
+  //   (async () => {
+  //     // Request permission to access location
+  //     const { status } = await Location.requestForegroundPermissionsAsync();
+  //     if (status !== 'granted') {
+  //       setErrorMsg('Permission to access location was denied');
+  //       return;
+  //     }
+
+  //     // Get the user's location
+  //     const location = await Location.getCurrentPositionAsync({});
+  //     setLocation(location);
+  //   })();
+  // }, []);
+
+  // let text = 'Waiting..';
+  // if (errorMsg) {
+  //   text = errorMsg;
+  // } else if (location) {
+  //   text = JSON.stringify(location);
+  // }
+
   useEffect(() => {
-    (async () => {
-      // Request permission to access location
-      const { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
+    getNearbyCategories('hotels');
+    console.log('ran');
+  }, [location]);
 
-      // Get the user's location
-      const location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
+  const getNearbyCategories = async (category: Category) => {
+    const options = {
+      method: 'GET',
+      url: 'https://api.content.tripadvisor.com/api/v1/location/nearby_search',
+      params: {
+        latLong: '42.3455%2C-71.10767',
+        // key: process.env.TRIPADVISOR_API_KEY,
+        key: 'C653ED7F198341E28C521B15D4DF4AC1',
+        category,
+        language: 'en',
+      },
+      headers: { accept: 'application/json' },
+    };
 
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
+    const response = await axios.request(options).catch((error) => {
+      console.error(error);
+    });
+
+    if (response) {
+      setHotels(response?.data);
+    }
+
+    // console.log(JSON.stringify(response?.data));
+  };
 
   const HotelCard = ({ hotel }: { hotel: any }) => {
     return (
@@ -60,6 +105,22 @@ export default function Home() {
     );
   };
 
+  const HorizontalScroll = ({ children }: { children: React.ReactNode }) => {
+    return (
+      <ScrollView style={styles.scrollView} horizontal showsHorizontalScrollIndicator={false}>
+        {children}
+      </ScrollView>
+    );
+  };
+
+  const TripAdvisorDestinationCard = ({ destination }: { destination: any }) => {
+    return (
+      <View style={styles.hotelCard}>
+        <Text>{destination.name}</Text>
+      </View>
+    );
+  };
+
   return (
     <>
       <Stack.Screen options={{ title: 'Tab Two' }} />
@@ -70,7 +131,12 @@ export default function Home() {
           ))}
         </ScrollView>
         <Spacer size={10} vertical />
-        <Text>{text}</Text>
+        <Text>Nearby Hotels</Text>
+        <HorizontalScroll>
+          {hotels?.data?.map((hotel: Hotel, index: number) => (
+            <HotelCard key={`${hotel.dest_id}-${index}`} hotel={hotel} />
+          ))}
+        </HorizontalScroll>
       </ScrollView>
     </>
   );
