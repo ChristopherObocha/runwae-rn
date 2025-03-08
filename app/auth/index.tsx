ccaf13426c49aa87a451ab87e9ebb3be2a59042d
Christopher Obocha <christopherobocha@gmail.com>
2025-03-08 18:29:00 +0000

    login screen UI partially complete

--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -1,113 +1,147 @@
-import { LinearGradient } from 'expo-linear-gradient';
-import { Link } from 'expo-router';
+import { Canvas, RadialGradient, Rect, vec } from '@shopify/react-native-skia';
 import * as React from 'react';
-import { Image, Platform, StyleSheet, View } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import { Logo } from '~/components/Logo';
-import { AlertAnchor } from '~/components/nativewindui/Alert';
 import { AlertRef } from '~/components/nativewindui/Alert/types';
-import { Button } from '~/components/nativewindui/Button';
 import { Text } from '~/components/nativewindui/Text';
-import { styles } from '~/components/onboarding/slide';
 import { useColorScheme } from '~/lib/useColorScheme';
-import { COLORS } from '~/theme/colors';
-
-const GOOGLE_SOURCE = {
-  uri: 'https://www.pngall.com/wp-content/uploads/13/Google-Logo.png',
-};
+import { appColors } from '~/utils/styles';
 
 export default function AuthIndexScreen() {
   const alertRef = React.useRef<AlertRef>(null);
   const insets = useSafeAreaInsets();
   const colors = useColorScheme().colors;
+  const { width, height } = Dimensions.get('window');
+
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [isInputFocused, setIsInputFocused] = React.useState(false);
+  const [isPasswordFocused, setIsPasswordFocused] = React.useState(false);
 
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: Platform.OS === 'ios' ? 'flex-end' : 'center',
-      gap: 16,
-      marginHorizontal: 32,
+      paddingHorizontal: 20,
+      marginTop: insets.top + 20,
       marginBottom: insets.bottom + 20,
-    },
-    linearGradient: {
-      flex: 1,
-      marginHorizontal: -32,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderBottomLeftRadius: 52,
-      borderBottomRightRadius: 52,
     },
     contentContainer: {
       flex: 1,
-      paddingHorizontal: 32,
+      justifyContent: 'space-between',
+    },
+    headerContainer: {
+      paddingTop: 40,
+    },
+    canvas: {
+      ...StyleSheet.absoluteFillObject,
+    },
+    button: {
+      backgroundColor: '#FFF',
+      paddingVertical: 15,
+      paddingHorizontal: 30,
+      borderRadius: 12,
+      alignItems: 'center',
+    },
+    buttonText: {
+      color: '#EA5809',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+    title: {
+      fontSize: 40,
+      fontWeight: 'bold',
+      fontFamily: 'Inter',
+      color: appColors.white,
+      lineHeight: 48,
+    },
+    description: {
+      fontSize: 16,
+      fontFamily: 'Inter_500Medium',
+      color: appColors.white,
+      marginTop: 8,
+      lineHeight: 24,
+    },
+    input: {
+      backgroundColor: 'transparent',
+      borderWidth: 1,
+      borderColor: appColors.white,
+      borderRadius: 12,
+      paddingHorizontal: 16,
+      paddingVertical: 12,
+      height: 50,
+      color: appColors.white,
+    },
+    inputFocused: {
+      borderColor: appColors.white,
+      borderWidth: 2,
+    },
+    inputContainer: {
+      gap: 16,
+      paddingBottom: 20,
     },
   });
 
+  const NextButton = () => {
+    return (
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <>
+      <Canvas style={styles.canvas}>
+        <Rect x={0} y={0} width={width} height={height}>
+          <RadialGradient
+            c={vec(width + 50, height / 2)}
+            r={height * 0.6}
+            colors={[appColors.white, appColors.gradientOrange]}
+          />
+        </Rect>
+      </Canvas>
+
       <View style={styles.container}>
-        <LinearGradient
-          colors={[COLORS.gradientOne, COLORS.gradientTwo]}
-          style={styles.linearGradient}>
-          <View className="items-center">
-            <Logo />
+        <View style={styles.contentContainer}>
+          <View style={styles.headerContainer}>
+            <Text style={styles.title}>Get Started.</Text>
+            <Text style={styles.description}>
+              Your journey begins here. Let's make travel simple and exciting!
+            </Text>
           </View>
-        </LinearGradient>
 
-        <View className="ios:pb-5 ios:pt-2 pb-2">
-          <Text className="ios:font-extrabold text-center text-3xl font-medium">
-            Brace Yourself
-          </Text>
-          <Text className="ios:font-extrabold text-center text-3xl font-medium">
-            for What's Next
-          </Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+              placeholder="Email"
+              placeholderTextColor={appColors.white}
+              value={email}
+              onChangeText={setEmail}
+              style={[styles.input, isInputFocused && styles.inputFocused]}
+              onFocus={() => setIsInputFocused(true)}
+              onBlur={() => setIsInputFocused(false)}
+            />
+            <TextInput
+              placeholder="Password"
+              placeholderTextColor={appColors.white}
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+              style={[styles.input, isPasswordFocused && styles.inputFocused]}
+              onFocus={() => setIsPasswordFocused(true)}
+              onBlur={() => setIsPasswordFocused(false)}
+            />
+            <NextButton />
+          </View>
         </View>
-
-        <Link href="/auth/(create-account)" asChild>
-          <Button
-            size={Platform.select({ ios: 'lg', default: 'md' })}
-            style={{ backgroundColor: COLORS.gradientOne }}>
-            <Text>Sign up free</Text>
-          </Button>
-        </Link>
-        <Button
-          variant="secondary"
-          className="ios:border-foreground/60"
-          size={Platform.select({ ios: 'lg', default: 'md' })}
-          onPress={() => {
-            alertRef.current?.alert({
-              title: 'Suggestion',
-              message: 'Use @react-native-google-signin/google-signin',
-              buttons: [{ text: 'OK', style: 'cancel' }],
-            });
-          }}>
-          <Image source={GOOGLE_SOURCE} className="absolute left-4 h-4 w-4" resizeMode="contain" />
-          <Text className="ios:text-foreground">Continue with Google</Text>
-        </Button>
-        {Platform.OS === 'ios' && (
-          <Button
-            variant="secondary"
-            className="ios:border-foreground/60"
-            size={Platform.select({ ios: 'lg', default: 'md' })}
-            onPress={() => {
-              alertRef.current?.alert({
-                title: 'Suggestion',
-                message: 'Use expo-apple-authentication',
-                buttons: [{ text: 'OK', style: 'cancel' }],
-              });
-            }}>
-            <Text className="ios:text-foreground absolute left-4 text-[22px]"></Text>
-            <Text className="ios:text-foreground">Continue with Apple</Text>
-          </Button>
-        )}
-        <Link href="/auth/(login)" asChild>
-          <Button variant="plain" size={Platform.select({ ios: 'lg', default: 'md' })}>
-            <Text className="text-primary">Log in</Text>
-          </Button>
-        </Link>
       </View>
-      <AlertAnchor ref={alertRef} />
+      {/* <AlertAnchor ref={alertRef} /> */}
     </>
   );
 }
