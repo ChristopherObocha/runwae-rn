591dbdc419c3dc63341ec5dbb59446b5f8015745
Christopher Obocha <christopherobocha@gmail.com>
2025-04-26 22:03:15 +0100

    added missing files

---
+++ cache.ts
@@ -0,0 +1,27 @@
+import * as SecureStore from 'expo-secure-store';
+import { Platform } from 'react-native';
+import { TokenCache } from '@clerk/clerk-expo/dist/cache';
+
+const createTokenCache = (): TokenCache => {
+  return {
+    getToken: async (key: string) => {
+      try {
+        const item = await SecureStore.getItemAsync(key);
+        if (item) {
+          console.log('Token found in cache', key);
+        } else {
+          console.log('No values found under key', key);
+        }
+        return item;
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+    },
+    saveToken: (key: string, token: string) => {
+      return SecureStore.setItemAsync(key, token);
+    },
+  };
+};
+
+export const tokenCache = Platform.OS !== 'web' ? createTokenCache() : undefined;
