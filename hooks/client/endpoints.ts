ab08766af48ee8cb92fb0cd215e0f24b91f91464
Christopher Obocha <christopherobocha@gmail.com>
2025-05-23 05:47:31 +0100

    added endpoints

--- hooks/client/endpoints.ts
+++ hooks/client/endpoints.ts
@@ -1,3 +1,5 @@
 export const ENDPOINTS = {
   baseurl: 'https://runwae.com/api',
+  baseAmadeus: 'https://test.api.amadeus.com/',
+  amadeusGetToken: 'https://test.api.amadeus.com/v1/security/oauth2/token',
 };
